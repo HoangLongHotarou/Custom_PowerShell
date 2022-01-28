91d55d70c754c54602082f1412c4cd350e018f8a
HoangLongHotarou <71645795+HoangLongHotarou@users.noreply.github.com>
2022-01-28 21:17:10 +0700

    Update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,12 +1,5 @@
-Import-Module posh-git
-Import-Module oh-my-posh
-#Set-PoshPrompt -Theme powerlevel10k_lean
-#Set-PoshPrompt -Theme huvix
-#Set-PoshPrompt -Theme material
-#Set-PoshPrompt -Theme microverse-power
-#Set-PoshPrompt -Theme zash
-Set-PoshPrompt -Theme slimfat
-
+#Update
+Invoke-Expression (oh-my-posh --init --shell pwsh --config "$(scoop prefix oh-my-posh)/themes/space.omp.json")
 
 Import-Module PSReadLine
 
@@ -19,3 +12,26 @@ Set-PSReadLineOption -ShowToolTips
 Set-PSReadLineOption -PredictionSource History
 
 Set-PSReadLineOption -Colors @{ InlinePrediction = '#9999ff'}
+
+
+# Import-Module posh-git
+# Import-Module oh-my-posh
+# #Set-PoshPrompt -Theme powerlevel10k_lean
+# #Set-PoshPrompt -Theme huvix
+# #Set-PoshPrompt -Theme material
+# #Set-PoshPrompt -Theme microverse-power
+# #Set-PoshPrompt -Theme zash
+# Set-PoshPrompt -Theme slimfat
+
+
+# Import-Module PSReadLine
+
+# Set-PSReadLineKeyHandler -Key Tab -Function Complete
+
+# Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+# Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+# Set-PSReadLineOption -ShowToolTips
+# Set-PSReadLineOption -PredictionSource History
+
+# Set-PSReadLineOption -Colors @{ InlinePrediction = '#9999ff'}
